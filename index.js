a13f6a238adc1bd55594009b2912935ab13a7809
Justinas <gercasjustinas@gmail.com>
2024-03-26 13:44:59 +0100

    stuck on preventing website from reloading

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const form = document.querySelector('#form')
+const list = document.querySelector('.list')
+const myInput = document.querySelector('#my-input')
+const email = document.querySelector('#email')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
 
